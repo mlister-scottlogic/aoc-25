f8729724de1e44bba3abaf86d146c484990673b8
Mark Lister <mlister@scottlogic.com>
2025-12-01 11:47:02 +0000

    Add Silas timer

--- day1/main.py
+++ day1/main.py
@@ -1,47 +1,63 @@
+import time
+from functools import wraps
+
+def timeit(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"{func.__name__} took {end - start:.6f} seconds")
+        return result
+    return wrapper
+
 def get_input(filename):
     text_file = open(filename, "r")
     return text_file.readlines()
 
-dial_history = []
+@timeit
+def part2():
+    dial_history = []
 
-dial = 50
+    dial = 50
 
-dial_max = 100
+    dial_max = 100
 
-dial_0s = 0
+    dial_0s = 0
 
-dial_history.append(dial)
-
-# input = get_input("example.txt")
-input = get_input("input.txt")
-
-for i in input: 
-    if (i[0] == "L"):
-        old_dial = dial
-        dial -= int(i[1:])
-
-        absv = abs(dial // dial_max)
-        dial = dial % dial_max
-
-        if (dial == 0):
-            dial_0s += 1
-
-        if (old_dial == 0 and absv > 0):
-            dial_0s += absv -1
-        else:
-            dial_0s += absv
-            
-    if (i[0] == "R"):
-        dial += int(i[1:])
-        dial_0s += abs(dial // dial_max)
-        dial = dial % dial_max
-
-    print(i.strip())
-    print("dial ", dial)
-    print("times 0 ", dial_0s)
     dial_history.append(dial)
 
-print("Answer:")
-print(dial_0s)
+    # input = get_input("example.txt")
+    input = get_input("input.txt")
 
+    for i in input: 
+        if (i[0] == "L"):
+            old_dial = dial
+            dial -= int(i[1:])
 
+            absv = abs(dial // dial_max)
+            dial = dial % dial_max
+
+            if (dial == 0):
+                dial_0s += 1
+
+            if (old_dial == 0 and absv > 0):
+                dial_0s += absv -1
+            else:
+                dial_0s += absv
+                
+        if (i[0] == "R"):
+            dial += int(i[1:])
+            dial_0s += abs(dial // dial_max)
+            dial = dial % dial_max
+
+        # print(i.strip())
+        # print("dial ", dial)
+        # print("times 0 ", dial_0s)
+        dial_history.append(dial)
+
+    # print("Answer:")
+    # print(dial_0s)
+    return dial_0s
+
+print(part2())
