0d1fc931bb9da40384ff75aa8dda2177e4c39531
Mark Lister <mlister@scottlogic.com>
2025-12-01 11:25:40 +0000

    Working sln

--- day1/main.py
+++ day1/main.py
@@ -8,21 +8,40 @@ dial = 50
 
 dial_max = 100
 
+dial_0s = 0
+
 dial_history.append(dial)
 
-input = get_input("input1.txt")
+# input = get_input("example.txt")
+input = get_input("input.txt")
 
 for i in input: 
     if (i[0] == "L"):
+        old_dial = dial
         dial -= int(i[1:])
+
+        absv = abs(dial // dial_max)
+        dial = dial % dial_max
+
+        if (dial == 0):
+            dial_0s += 1
+
+        if (old_dial == 0 and absv > 0):
+            dial_0s += absv -1
+        else:
+            dial_0s += absv
+            
     if (i[0] == "R"):
         dial += int(i[1:])
-    dial = dial % dial_max
-    # print(i)
-    # print(dial)
+        dial_0s += abs(dial // dial_max)
+        dial = dial % dial_max
+
+    print(i.strip())
+    print("dial ", dial)
+    print("times 0 ", dial_0s)
     dial_history.append(dial)
 
 print("Answer:")
-print(dial_history.count(0))
+print(dial_0s)
 
 
