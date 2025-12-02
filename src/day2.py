2d90c18a02078f52aecf405a57bc19fe45e342c8
Mark Lister <mlister@scottlogic.com>
2025-12-02 10:44:55 +0000

    Working part 1

--- src/day2.py
+++ src/day2.py
@@ -1,4 +1,5 @@
 from support.timers import timeit
+import math
 
 
 def get_input(filename):
@@ -11,10 +12,35 @@ def get_input(filename):
 
 @timeit
 def part1(input_file: str):
-    input = get_input(input_file)
-    print(input)
-
+    inputs = get_input(input_file)
+    print(inputs)
     count = 0
+
+    for input in inputs:
+        # print(input)
+
+        lower = int(input[0])
+        higher = int(input[1])
+
+        for j in range(lower, higher + 1):
+            value_to_check = str(j)
+            ## If odd number it can't be 2 repeating patterns
+            if len(value_to_check) % 2 == 0:
+                half_point = math.floor(len(value_to_check) / 2)
+                # print(value_to_check)
+                # print(half_point)
+
+                first_part = value_to_check[0:half_point]
+                second_part = value_to_check[half_point:]
+
+                # print(first_part)
+                # print(second_part)
+
+                if first_part == second_part:
+                    # print(first_part)
+                    # print(second_part)
+                    # print(value_to_check)
+                    count += j
 
     return count
 
@@ -26,7 +52,7 @@ def part2(input_file: str):
 
 
 print("Part 1")
-print(part1("day2/example.txt"))
+# print(part1("day2/example.txt"))
 print(part1("day2/input.txt"))
 
 # print("Part 2")
