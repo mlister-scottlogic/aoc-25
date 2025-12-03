422a0f7ca0b10e311eb949adb84a1a74388b885e
Mark Lister <mlister@scottlogic.com>
2025-12-03 11:18:30 +0000

    Failed part 2

--- src/day3.py
+++ src/day3.py
@@ -10,16 +10,36 @@ def get_input(filename):
     return [[int(c) for c in l] for l in all_lines]
 
 
-def joltage(batteries: List[int]):
+def joltage1(batteries: List[int]):
     if len(batteries) == 2:
         return (batteries[0], batteries[1])
 
-    current_best = joltage(batteries[1:])
+    current_best = joltage1(batteries[1:])
     if batteries[0] >= current_best[0]:
         if current_best[0] > current_best[1]:
             return (batteries[0], current_best[0])
         else:
             return (batteries[0], current_best[1])
+
+    return current_best
+
+
+def joltage2(batteries: List[int], battery_size: int):
+    if len(batteries) == battery_size:
+        # print(batteries[:battery_size])
+        return batteries
+
+    current_best = joltage2(batteries[1:], battery_size)
+
+    current_min = min(current_best)
+
+    ## Turn off earliest occurence of minimum?
+    # print(f"--- {batteries[0]} --- {current_best} --- {batteries} --- {current_min}")
+
+    if batteries[0] >= current_best[0]:
+        current_best.remove(current_min)
+
+        return [batteries[0], *current_best]
 
     return current_best
 
@@ -32,8 +52,9 @@ def part1(input_file: str):
     # print(inputs)
 
     for i in inputs:
-        best_value = joltage(i)
-        jolt = best_value[0] * 10 + best_value[1]
+        best_value = joltage2(i, 2)
+        best_value_strings = [str(i) for i in best_value]
+        jolt = int("".join(best_value_strings))
         # print(f"------ {i} --- {best_value} --- {jolt} ------")
         total += jolt
 
@@ -42,15 +63,27 @@ def part1(input_file: str):
 
 @timeit
 def part2(input_file: str):
-    inputs = get_input(input_file)
     # print(inputs)
-    count = 0
+    total = 0
+    inputs = get_input(input_file)
+
+    # print(inputs)
+
+    for i in inputs:
+        best_value = joltage2(i, 12)
+        # print(best_value)
+        best_value_strings = [str(i) for i in best_value]
+        jolt = int("".join(best_value_strings))
+        print(f"------ {i} --- {best_value} --- {jolt} ------")
+        total += jolt
+
+    return total
 
 
 print("Part 1")
 # print(part1("day3/example.txt"))
-print(part1("day3/input.txt"))
+# print(part1("day3/input.txt"))
 
 print("Part 2")
-# print(part2("day3/example.txt"))
+print(part2("day3/example.txt"))
 # print(part2("day3/input.txt"))
