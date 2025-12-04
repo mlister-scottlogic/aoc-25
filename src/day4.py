2d525f56cefbc0b81910a4d332696018d89388e7
Mark Lister <mlister@scottlogic.com>
2025-12-04 10:03:21 +0000

    Day 4 part 1

--- src/day4.py
+++ src/day4.py
@@ -20,7 +20,29 @@ def part1(input_file: str):
     total = 0
     inputs = get_input(input_file)
 
-    print(inputs)
+    rolls_points = {}
+
+    for point in inputs:
+        # For all rolls
+        if inputs[point]:
+            nearby_rolls = 0
+            for x in range(-1, 2):
+                for y in range(-1, 2):
+                    # ignore 0,0
+                    if not (x == 0 and y == 0):
+                        point_to_check = (point[0] + y, point[1] + x)
+
+                        roll_to_check = inputs.get(point_to_check, None)
+
+                        if roll_to_check is not None and roll_to_check:
+                            nearby_rolls += 1
+            rolls_points[point] = nearby_rolls
+
+    # print(rolls_points)
+
+    for point in rolls_points:
+        if rolls_points[point] < 4:
+            total += 1
 
     return total
 
@@ -35,7 +57,7 @@ def part2(input_file: str):
 
 print("Part 1")
 print(part1("day4/example.txt"))
-# print(part1("day4/input.txt"))
+print(part1("day4/input.txt"))
 
 print("Part 2")
 # print(part2("day4/example.txt"))
