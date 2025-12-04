58527e7fce7612a4dbc7220b5dbc3b6c9a1a71d3
Mark Lister <mlister@scottlogic.com>
2025-12-04 15:54:00 +0000

    Use ints rather than bools

--- src/day4.py
+++ src/day4.py
@@ -2,7 +2,7 @@ from typing import List
 from support.timers import timeit
 
 
-def get_input(filename) -> dict[tuple[int, int], bool]:
+def get_input(filename) -> dict[tuple[int, int], int]:
     text_file = open(filename, "r")
     all_lines = text_file.readlines()
     all_lines = [l.strip() for l in all_lines]
@@ -10,46 +10,23 @@ def get_input(filename) -> dict[tuple[int, int], bool]:
     roll_map = {}
     for y, line in enumerate(all_lines):
         for x, field in enumerate(line):
-            roll_map[(x, y)] = field == "@"
+            roll_map[(x, y)] = 1 if field == "@" else 0
 
     return roll_map
 
 
 @timeit
 def part1(input_file: str):
-    total = 0
     inputs = get_input(input_file)
 
-    rolls_points = {}
-
-    for point in inputs:
-        # For all rolls
-        if inputs[point]:
-            nearby_rolls = 0
-            for x in range(-1, 2):
-                for y in range(-1, 2):
-                    # ignore 0,0
-                    if not (x == 0 and y == 0):
-                        point_to_check = (point[0] + y, point[1] + x)
-
-                        roll_to_check = inputs.get(point_to_check, None)
-
-                        if roll_to_check is not None and roll_to_check:
-                            nearby_rolls += 1
-            rolls_points[point] = nearby_rolls
-
-    # print(rolls_points)
-
-    for point in rolls_points:
-        if rolls_points[point] < 4:
-            total += 1
+    total, new_map = calculate_rolls_removed(inputs)
 
     return total
 
 
 def calculate_rolls_removed(
-    input_map: dict[tuple[int, int], bool],
-) -> tuple[int, dict[tuple[int, int], bool]]:
+    input_map: dict[tuple[int, int], int],
+) -> tuple[int, dict[tuple[int, int], int]]:
     rolls_points = {}
 
     for point in input_map:
@@ -63,20 +40,17 @@ def calculate_rolls_removed(
                     if not (x_delta == 0 and y_delta == 0):
                         point_to_check = (x + x_delta, y + y_delta)
 
-                        roll_to_check = input_map.get(point_to_check, None)
+                        roll_to_check = input_map.get(point_to_check, 0)
 
-                        if roll_to_check is not None and roll_to_check:
-                            nearby_rolls += 1
+                        nearby_rolls += roll_to_check
             rolls_points[point] = nearby_rolls
-
-    # print(rolls_points)
 
     total = 0
 
     for point in rolls_points:
         if rolls_points[point] < 4:
             total += 1
-            input_map[point] = False
+            input_map[point] = 0
 
     return total, input_map
 
@@ -90,7 +64,6 @@ def part2(input_file: str):
 
     while rolls_removed != 0:
         rolls_removed, inputs = calculate_rolls_removed(inputs)
-        # print(rolls_removed)
         total += rolls_removed
 
     return total
@@ -98,7 +71,7 @@ def part2(input_file: str):
 
 print("Part 1")
 # print(part1("day4/example.txt"))
-# print(part1("day4/input.txt"))
+print(part1("day4/input.txt"))
 
 print("Part 2")
 # print(part2("day4/example.txt"))
