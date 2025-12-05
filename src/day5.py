a22ebc20da27a5a7599c8f3143970d98a1c95a04
Mark Lister <mlister@scottlogic.com>
2025-12-05 13:36:14 +0000

    Use sorted set

--- src/day5.py
+++ src/day5.py
@@ -1,6 +1,8 @@
 from typing import List
 from support.timers import timeit
 
+from sortedcontainers import SortedSet
+
 
 def get_input(filename) -> tuple[List[tuple[int, int]], List[int]]:
     text_file = open(filename, "r")
@@ -16,6 +18,29 @@ def get_input(filename) -> tuple[List[tuple[int, int]], List[int]]:
     ids = [int(id) for id in all_lines[space_index + 1 :]]
 
     return ranges, ids
+
+
+class IdRange:
+    def __init__(self, lower, upper):
+        if upper < lower:
+            raise ValueError
+
+        self.lower = lower
+        self.upper = upper
+
+    def __repr__(self) -> str:
+        return f"({self.lower},{self.upper})"
+
+    def __eq__(self, other):
+        if not isinstance(other, IdRange):
+            return NotImplemented
+        return self.lower == other.lower and self.upper == other.upper
+
+    def __hash__(self):
+        return hash(self.__repr__())
+
+    def __lt__(self, other):
+        return self.lower < other.lower
 
 
 @timeit
@@ -38,51 +63,39 @@ def part1(input_file: str):
 def part2(input_file: str):
     ranges, _ = get_input(input_file)
 
-    ranges2 = [dict(lower=r[0], upper=r[1]) for r in ranges]
-
-    ranges2.sort(key=lambda r: r["lower"])
-
-    # print(ranges2)
+    ranges2 = SortedSet([IdRange(r[0], r[1]) for r in ranges])
 
     changed = True
 
     while changed:
-        # print("------ new iteration ------")
         changed = False
 
         for i in range(1, len(ranges2)):
             first_bound = ranges2[i - 1]
             second_bound = ranges2[i]
 
-            if first_bound["upper"] >= second_bound["lower"]:
+            if first_bound.upper >= second_bound.lower:
                 if (
-                    second_bound["upper"] < first_bound["upper"]
+                    second_bound.upper < first_bound.upper
                     and first_bound != second_bound
                 ):
-                    second_bound["upper"] = first_bound["upper"]
-                    second_bound["lower"] = first_bound["lower"]
+                    ranges2.pop(i)
 
                     changed = True
+
+                    break
                 elif first_bound != second_bound:
-                    first_bound["upper"] = second_bound["upper"]
-                    second_bound["lower"] = first_bound["lower"]
+                    ranges2.remove(first_bound)
+                    ranges2.remove(second_bound)
+                    ranges2.add(IdRange(first_bound.lower, second_bound.upper))
 
                     changed = True
-
-        # Remove duplicates
-        ranges_set = set([(r["lower"], r["upper"]) for r in ranges2])
-        ranges2 = [dict(lower=r[0], upper=r[1]) for r in ranges_set]
-        ranges2.sort(key=lambda r: r["lower"])
-
-        # print(ranges2)
+                    break
 
     total = 0
-    ranges_set = set([(r["lower"], r["upper"]) for r in ranges2])
 
-    # print(ranges_set)
-
-    for r in ranges_set:
-        total += (r[1] + 1) - r[0]
+    for r in ranges2:
+        total += (r.upper + 1) - r.lower
 
     return total
 
@@ -92,5 +105,5 @@ print("Part 1")
 # print(part1("day5/input.txt"))
 
 print("Part 2")
-print(part2("day5/example.txt"))
+# print(part2("day5/example.txt"))
 print(part2("day5/input.txt"))
