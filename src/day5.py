a09c910d38e9127bac37850527cfb157766df76c
Mark Lister <mlister@scottlogic.com>
2025-12-05 11:38:15 +0000

    Part 2

--- src/day5.py
+++ src/day5.py
@@ -25,8 +25,8 @@ def part1(input_file: str):
     total = 0
 
     for id in ids:
-        for range in ranges:
-            if id >= range[0] and id <= range[1]:
+        for r in ranges:
+            if id >= r[0] and id <= r[1]:
                 total += 1
                 # print(id)
                 break
@@ -36,13 +36,61 @@ def part1(input_file: str):
 
 @timeit
 def part2(input_file: str):
-    inputs = get_input(input_file)
+    ranges, _ = get_input(input_file)
+
+    ranges2 = [dict(lower=r[0], upper=r[1]) for r in ranges]
+
+    ranges2.sort(key=lambda r: r["lower"])
+
+    # print(ranges2)
+
+    changed = True
+
+    while changed:
+        # print("------ new iteration ------")
+        changed = False
+
+        for i in range(1, len(ranges2)):
+            first_bound = ranges2[i - 1]
+            second_bound = ranges2[i]
+
+            if first_bound["upper"] >= second_bound["lower"]:
+                if (
+                    second_bound["upper"] < first_bound["upper"]
+                    and first_bound != second_bound
+                ):
+                    second_bound["upper"] = first_bound["upper"]
+                    second_bound["lower"] = first_bound["lower"]
+
+                    changed = True
+                elif first_bound != second_bound:
+                    first_bound["upper"] = second_bound["upper"]
+                    second_bound["lower"] = first_bound["lower"]
+
+                    changed = True
+
+        # Remove duplicates
+        ranges_set = set([(r["lower"], r["upper"]) for r in ranges2])
+        ranges2 = [dict(lower=r[0], upper=r[1]) for r in ranges_set]
+        ranges2.sort(key=lambda r: r["lower"])
+
+        # print(ranges2)
+
+    total = 0
+    ranges_set = set([(r["lower"], r["upper"]) for r in ranges2])
+
+    # print(ranges_set)
+
+    for r in ranges_set:
+        total += (r[1] + 1) - r[0]
+
+    return total
 
 
 print("Part 1")
 # print(part1("day5/example.txt"))
-print(part1("day5/input.txt"))
+# print(part1("day5/input.txt"))
 
 print("Part 2")
-# print(part2("day5/example.txt"))
-# print(part2("day5/input.txt"))
+print(part2("day5/example.txt"))
+print(part2("day5/input.txt"))
