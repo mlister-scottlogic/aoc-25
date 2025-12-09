47b923febb566328cea823c5e4f834bde93c0f36
Mark <mlister@scottlogic.com>
2025-12-09 11:54:40 +0000

    Input parsing

--- src/day8.py
+++ src/day8.py
@@ -6,13 +6,17 @@ def get_input(filename):
     text_file = open(filename, "r")
     all_lines = text_file.readlines()
 
-    return all_lines
+    parsed = [[int(x) for x in l.strip().split(",")] for l in all_lines]
+
+    return [(c[0], c[1], c[2]) for c in parsed]
 
 
 @timeit
 def part1(input_file: str):
     total = 0
     input = get_input(input_file)
+
+    print(input)
 
     return total
 
@@ -26,7 +30,7 @@ def part2(input_file: str):
 
 
 print("Part 1")
-# print(part1("day8/example.txt"))
+print(part1("day8/example.txt"))
 # print(part1("day8/input.txt"))
 
 print("Part 2")
