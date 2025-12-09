7b073f5f1a28d87ffa657777d10a87348416ddba
Mark <mlister@scottlogic.com>
2025-12-09 13:08:01 +0000

    Very slow part 2

--- src/day8.py
+++ src/day8.py
@@ -61,16 +61,53 @@ def part1(input_file: str, iterations):
 
 @timeit
 def part2(input_file: str):
-    total = 0
     input = get_input(input_file)
+
+    circuits = [set([i]) for i in input]
+
+    minimums = dict()
+
+    for p1 in input:
+        minimum_for_point = [((p1, p2), distance(p1, p2)) for p2 in input if p1 != p2]
+        # print(p1, minimum)
+        for points, d in minimum_for_point:
+            minimums[frozenset([*points])] = d
+
+    res = None
+
+    while len(circuits) > 1:
+        overall_minimum = min(minimums.values())
+        res = [k for k, v in minimums.items() if v == overall_minimum][0]
+
+        # print(res)
+
+        related_circuits = [s for s in circuits if len(s.intersection(res)) > 0]
+
+        # If they aren't already connected merge the sets
+        if len(related_circuits) > 1:
+            # print(related_circuits)
+            circuits.remove(related_circuits[0])
+            related_circuits[1].update(related_circuits[0])
+
+        # print(circuits, len(circuits))
+
+        # remove element we just worked on
+        minimums.pop(res)
+
+    print(res)
+    total = 1
+
+    for x in res:
+        print(x)
+        total = total * x[0]
 
     return total
 
 
 print("Part 1")
 # print(part1("day8/example.txt", 10))
-print(part1("day8/input.txt", 1000))
+# print(part1("day8/input.txt", 1000))
 
 print("Part 2")
 # print(part2("day8/example.txt"))
-# print(part2("day8/input.txt"))
+print(part2("day8/input.txt"))
