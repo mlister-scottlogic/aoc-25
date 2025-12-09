d97365dc717e818aa5e699590289dc7a31ab73a5
Mark <mlister@scottlogic.com>
2025-12-09 10:24:37 +0000

    Day 6 part 2

--- src/day6.py
+++ src/day6.py
@@ -8,9 +8,6 @@ def get_input(filename):
     text_file = open(filename, "r")
     all_lines = text_file.readlines()
 
-    rex = re.compile(r"\s+")
-    all_lines = [rex.sub(" ", l.strip()).split(" ") for l in all_lines]
-
     return all_lines
 
 
@@ -18,6 +15,9 @@ def get_input(filename):
 def part1(input_file: str):
     total = 0
     inputs = get_input(input_file)
+
+    rex = re.compile(r"\s+")
+    inputs = [rex.sub(" ", l.strip()).split(" ") for l in inputs]
 
     # print(inputs)
 
@@ -28,7 +28,7 @@ def part1(input_file: str):
             # print(i)
             numbers.append(int(inputs[i][index]))
 
-        # print(index, operator, numbers)
+        # print(row, operator, numbers)
 
         if operator == "+":
             s = sum(numbers)
@@ -46,12 +46,44 @@ def part1(input_file: str):
 @timeit
 def part2(input_file: str):
     inputs = get_input(input_file)
+    total = 0
+
+    current_operator = " "
+    numbers = []
+
+    for i in range(len(inputs[-1]) - 1, -1, -1):
+        current_operator = inputs[-1][i]
+
+        temp_num = ""
+        for row in inputs[:-1]:
+            temp_num += row[i]
+
+        if temp_num.strip() != "":
+            numbers.append(int(temp_num))
+
+        if current_operator != " ":
+            # print(current_operator, numbers)
+
+            if current_operator == "+":
+                s = sum(numbers)
+                # print(s)
+                total += s
+
+            if current_operator == "*":
+                p = math.prod(numbers)
+                # print(p)
+                total += p
+
+            # New problem starting after operator has been applied so reset numbers
+            numbers = []
+
+    return total
 
 
 print("Part 1")
-print(part1("day6/example.txt"))
-print(part1("day6/input.txt"))
+# print(part1("day6/example.txt"))
+# print(part1("day6/input.txt"))
 
 print("Part 2")
 # print(part2("day6/example.txt"))
-# print(part2("day6/input.txt"))
+print(part2("day6/input.txt"))
