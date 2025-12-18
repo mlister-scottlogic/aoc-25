6ad0cbcfa1f86a0072705dd1614c54c872282b55
Mark <mlister@scottlogic.com>
2025-12-18 14:11:56 +0000

    2020 day 1

--- src/2020/day1.py
+++ src/2020/day1.py
@@ -1,29 +1,47 @@
 from support.timers import timeit
 from collections import Counter
+from itertools import combinations
 
 
 def get_input(filename):
     text_file = open(filename, "r")
-    return text_file.readline().strip()
+    return [int(i.strip()) for i in text_file.readlines()]
 
 
 @timeit
 def part1(input_file):
     input = get_input(input_file)
 
-    return 0
+    current = 0
+
+    for i in input:
+        current = i
+
+        for j in input[0:i] + input[i + 1 :]:
+            if current + j == 2020:
+                print(current, j)
+                return current * j
+
+    return -1
 
 
 @timeit
 def part2(input_file):
     input = get_input(input_file)
 
-    return 0
+    combos = combinations(range(len(input)), 3)
+
+    for c in combos:
+        if sum([input[c[0]], input[c[1]], input[c[2]]]) == 2020:
+            print(input[c[0]], input[c[1]], input[c[2]])
+            return input[c[0]] * input[c[1]] * input[c[2]]
+
+    return -1
 
 
-print("Part 1")
-print(part1("day1/example.txt"))
-print(part1("day1/input.txt"))
+# print("Part 1")
+# print(part1("day1/example.txt"))
+# print(part1("day1/input.txt"))
 
 print("Part 2")
 print(part2("day1/example.txt"))
